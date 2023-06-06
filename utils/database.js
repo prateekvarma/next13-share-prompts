d266495220af39b8fc1aa3b9a66a1a765c595664
prateekvarma <prtk.varma@gmail.com>
2023-06-06 15:50:02 -0600

    Fixes to store user in to mongodb after successful sign in

--- utils/database.js
+++ utils/database.js
@@ -13,8 +13,8 @@ export const connectToDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: 'share_prompt',
-      newUserUrlParser: true,
-      useUnifiedTypology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
 
     isConnected = true;
