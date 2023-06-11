f8a2ef643c082df231b94813486631c27c3a78b1
prateekvarma <prtk.varma@gmail.com>
2023-06-10 22:09:17 -0600

    Able to create a new prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,38 +1,45 @@
 'use client';
 
-import Form from '@/components/Form';
 import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Form from '@components/Form';
 
 const CreatePrompt = () => {
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: '',
-    tag: '',
-  });
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: '', tag: '' });
 
   const createPrompt = async (e) => {
-    e.preventDeafult();
-    setSubmitting(true);
+    e.preventDefault();
+    setIsSubmitting(true);
 
     try {
       const response = await fetch('/api/prompt/new', {
         method: 'POST',
         body: JSON.stringify({
           prompt: post.prompt,
-          userId: session?.userId,
+          userId: session?.user.id,
           tag: post.tag,
         }),
       });
 
-      if(response.ok) {
-        router.push('/')
+      if (response.ok) {
+        router.push('/');
       }
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
     <Form
-      type='create'
+      type='Create'
       post={post}
       setPost={setPost}
       submitting={submitting}
--- components/Form.jsx
+++ components/Form.jsx
@@ -4,7 +4,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
     <section className='w-full max-w-full flex-start flex-col'>
       <h1 className='head_text text-left'>
-        <span className='blue_radiant'>{type}</span> Post
+        <span className='blue_gradiant'>{type}</span> Post
       </h1>
       <p className='desc text-left max-w-md'>
         {type} and share amazing prompts with the world, and let you imagination
