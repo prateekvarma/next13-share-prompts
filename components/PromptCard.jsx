a12b85de46be7b5ed6fd5467a443326ece99c63b
prateekvarma <prtk.varma@gmail.com>
2023-06-12 13:47:29 -0600

    Implemented the copy/paste functionality

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -8,6 +8,12 @@ import { usePathname, useRouter } from 'next/navigation';
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState('');
 
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(''), 3000); //reset copied state
+  };
+
   return (
     <div className='prompt_card'>
       <div className='flex justify-between items-start gap-5'>
@@ -28,10 +34,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             </p>
           </div>
         </div>
-        <div className='copy_btn' onClick={() => {}}>
+        <div className='copy_btn' onClick={handleCopy}>
           <Image
             src={
-              copied === post.promot
+              copied === post.prompt
                 ? '/assets/icons/tick.svg'
                 : '/assets/icons/copy.svg'
             }
